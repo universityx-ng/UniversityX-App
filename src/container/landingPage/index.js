b166927e04d407659fdfccaaf0cfd12fab437cb6
Dammie <bitbender1010@gmail.com>
2021-10-20 15:54:07 +0100

    added folders img, theme, containers

--- src/container/landingPage/index.js
+++ src/container/landingPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Box, Flex, Spacer } from "@chakra-ui/react"
+import { Container, Box } from "@chakra-ui/react"
 import style from './style.module.css';
 
 function Home() {
