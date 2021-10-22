dd411e5911e4a964a511969379c98fb71ae6b4f3
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-22 11:03:02 +0100

    update homepage

--- src/container/landingPage/index.js
+++ src/container/landingPage/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import { Container, Box } from "@chakra-ui/react"
-import style from './style.module.css';
+import React from "react";
+import { Container, Box } from "@chakra-ui/react";
+import style from "./style.module.css";
+import NavBar from "../navbar";
+import Hero from "../hero";
+import Partners from "../partners";
+import Whatx from "../whatX";
+import Who from "../who";
+import Features from "../features";
+import Contact from '../contact'
 
 function Home() {
-    return (
-      <Container>
-        <Box className={style.welcome_msg}> Welcome to UniversityX </Box>
-      </Container>
-    );
-  }
-  
-  export default Home;
+  return (
+    <div>
+      <div style={{ padding: "2rem 5rem", background: "#FFB4EB" }}>
+        <NavBar />
+        <Hero />
+      </div>
+        <Partners />
+        <Whatx />
+        <Who />
+        <Features />
+        <Contact />
+    </div>
+  );
+}
+
+export default Home;
